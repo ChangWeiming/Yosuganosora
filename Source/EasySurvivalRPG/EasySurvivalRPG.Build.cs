79e00e08f0b97166e8941448878379d128ea22f4
ChangWeiming <46313483+ChangWeiming@users.noreply.github.com>
2021-06-28 11:24:34 +0800

    Update EasySurvivalRPG.Build.cs

--- Source/EasySurvivalRPG/EasySurvivalRPG.Build.cs
+++ Source/EasySurvivalRPG/EasySurvivalRPG.Build.cs
@@ -7,10 +7,10 @@ public class EasySurvivalRPG : ModuleRules
 	public EasySurvivalRPG(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "AIModule" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
